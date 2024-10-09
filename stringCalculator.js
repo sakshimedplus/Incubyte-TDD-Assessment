3832aac277963d2ba3ca2a7e493b7bec35c9dc26
sakshi <sakshidubey4467@gmail.com>
2024-10-09 20:36:55 +0530

    custom delimiter function

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,12 +4,21 @@ function add(numbers) {
     }
     let total = 0;   
     let currentNumber = "";
-
+    let delimiter = [',', '\n'];  // Default delimiters
+    if (numbers.startsWith("//")) {
+         // Check if there's a custom delimiter declaration
+        const firstNewlineIndex = numbers.indexOf("\n");
+        if (firstNewlineIndex !== -1) {
+            delimiter = [numbers.substring(2, firstNewlineIndex)];
+            // Remove the delimiter declaration from the string
+            numbers = numbers.substring(firstNewlineIndex + 1);
+        }
+    }
     for (let i = 0; i < numbers.length; i++) {
         const char = numbers[i];
         if (char >= '0' && char <= '9') {
             currentNumber += char;
-        } else if (char === ',' || char === '\n') {     
+        } else if (delimiter.includes(char)) {     
              if (currentNumber) {
                 total += Number(currentNumber);
                 currentNumber = "";
