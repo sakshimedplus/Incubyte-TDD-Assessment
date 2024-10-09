338533a8560c36bcc7ef6f7cc3e9cad2b230ab26
sakshi <sakshidubey4467@gmail.com>
2024-10-09 19:00:14 +0530

    ad two numbers test case

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,3 +2,9 @@ const add=require('./stringCalculator');
 test('returns 0 for empty string',()=>{
     expect(add("")).toBe(0);
 });
+test('returns number for single number',()=>{
+    expect(add("5")).toBe(5);
+});
+test('returns sum for 2 number',()=>{
+    expect(add("5,3")).toBe(8);
+});
