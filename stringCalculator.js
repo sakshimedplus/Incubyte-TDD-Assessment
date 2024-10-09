88f99a4452b6bde2d702cacd09bb369fa7f805aa
sakshi <sakshidubey4467@gmail.com>
2024-10-09 19:25:07 +0530

    add upto two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,23 @@
-function add(numbers){
-    if(numbers===""){
-        return 0;
+function add(numbers) {
+    if (numbers === "") {
+        return 0; // Return 0 in case of an empty string
     }
-    return Number(numbers)
+    let total = 0;   
+    let currentNumber = "";
+
+    for (let i = 0; i < numbers.length; i++) {
+        const char = numbers[i];
+        if (char >= '0' && char <= '9') {
+            currentNumber += char;
+        } else if (char === ',') {          if (currentNumber) {
+                total += Number(currentNumber);
+                currentNumber = "";
+            }
+        } 
+    }
+    if (currentNumber) {
+        total += Number(currentNumber);
+    }
+    return total; 
 }
 module.exports=add;
